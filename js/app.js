7a60e7d2f9aac569c4e66ca859bbc2ac578f636a
andrey <andreygabrielben@gmail.com>
2025-10-15 22:21:12 -0300

    adicionar a função de mudar a claridade da imagem ao clicar do botão

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
 function alterarStatus(id) {
     let game = document.getElementById(`game-${id}`);
-    let imagem = game.querySelector(".dashboard__item__img")
-    let botão = game.querySelector(".dashboard__item__button")
+    let imagem = game.querySelector(".dashboard__item__img");
+    let botão = game.querySelector(".dashboard__item__button");
     
     // função de mudança do botão
 
     if (botão.classList.contains("dashboard__item__button--return")) {
-        botão.classList.remove("dashboard__item__button--return")
-        botão.innerHTML = "Alugar"
+        botão.classList.remove("dashboard__item__button--return");
+        botão.innerHTML = "Alugar";
     } else {
-        botão.classList.add("dashboard__item__button--return")
-        botão.innerHTML = "Devolver"
-    }
+        botão.classList.add("dashboard__item__button--return");
+        botão.innerHTML = "Devolver";
+    };
 
+    // Função de mudança da imagem
 
-
-}
-
+    if (imagem.classList.contains("dashboard__item__img--rented")) {
+        imagem.classList.remove("dashboard__item__img--rented");
+    } else {
+        imagem.classList.add("dashboard__item__img--rented");
+    };
+};
