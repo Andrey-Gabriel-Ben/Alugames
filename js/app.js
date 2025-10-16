94c1d8e41df4b8700f2780041ac4428bc19ea720
andrey <andreygabrielben@gmail.com>
2025-10-15 22:16:28 -0300

    adicionar a alteração no botão ao ser clicado

--- js/app.js
+++ js/app.js
@@ -3,6 +3,17 @@ function alterarStatus(id) {
     let imagem = game.querySelector(".dashboard__item__img")
     let botão = game.querySelector(".dashboard__item__button")
     
-    
+    // função de mudança do botão
+
+    if (botão.classList.contains("dashboard__item__button--return")) {
+        botão.classList.remove("dashboard__item__button--return")
+        botão.innerHTML = "Alugar"
+    } else {
+        botão.classList.add("dashboard__item__button--return")
+        botão.innerHTML = "Devolver"
+    }
+
+
 
 }
+
