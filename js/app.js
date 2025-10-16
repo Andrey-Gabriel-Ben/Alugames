1ee3e8a3ad59a67be5a577d24d0d2c649d8ac26b
andrey <andreygabrielben@gmail.com>
2025-10-15 21:10:23 -0300

    iniciar o projeto

--- js/app.js
+++ js/app.js
@@ -1,19 +1,8 @@
 function alterarStatus(id) {
-    let gameClicado = document.getElementById(`game-${id}`);
-    let imagem = gameClicado.querySelector('.dashboard__item__img');
-    let botao = gameClicado.querySelector('.dashboard__item__button');
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
-    
-    alert(nomeJogo.textContent);
-}
-
-/*function alterarStatus(id) {
     let game = document.getElementById(`game-${id}`);
     let imagem = game.querySelector(".dashboard__item__img")
     let botão = game.querySelector(".dashboard__item__button")
-    let nome = game.querySelector(".dashboard__item__button")
-
-    alert(nome.textContent);
+    
+    
 
 }
-*/
